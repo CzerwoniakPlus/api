3e0168776deedba67b9d7706db47bdaf1fe396a9
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-09-16 07:17:17 +0000

    fix: :bug: Automatic timetable URL extraction

--- src/utils/timetableURLExtractor.ts
+++ src/utils/timetableURLExtractor.ts
@@ -4,17 +4,25 @@ import jsdom from "jsdom";
 const getTimetableURL = async (): Promise<string | null> => {
   return new Promise((resolve, reject) => {
     try {
-      got(
-        "http://zs1rowecki.pl/index.php?option=com_content&view=article&id=733&Itemid=417"
-      ).then((response) => {
-        const { JSDOM } = jsdom;
-        const dom = new JSDOM(response.body).window.document;
-        const timetableURL = dom
-          .querySelector("div.item-page")
-          ?.querySelector("p")
+      got("http://zs1rowecki.pl").then((response) => {
+        const dom = new jsdom.JSDOM(response.body).window.document;
+        const timetablePageURL = dom
+          .querySelector("li.item-417")
           ?.querySelector("a")?.href;
-        if (timetableURL) {
-          resolve(`http://zs1rowecki.pl/${timetableURL}`);
+        if (timetablePageURL) {
+          got(`http://zs1rowecki.pl/${timetablePageURL}`).then((response) => {
+            const { JSDOM } = jsdom;
+            const dom = new JSDOM(response.body).window.document;
+            const timetableURL = dom
+              .querySelector("div.item-page")
+              ?.querySelector("p")
+              ?.querySelector("a")?.href;
+            if (timetableURL) {
+              resolve(`http://zs1rowecki.pl/${timetableURL}`);
+            } else {
+              resolve(null);
+            }
+          });
         } else {
           resolve(null);
         }
